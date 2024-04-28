9b97725b6ae7f0a13d02af229399739c82117b69
Allyson-SFelix <allysonstudy2021@gmail.com>
2024-04-28 00:22:06 -0300

    Possibilitou salvar os valores dentro da array

--- frame.py
+++ frame.py
@@ -40,22 +40,21 @@ class Aplicativo():
 
     def addNosFrames(self):
         #falta pensar na logica para colocar na posicao certa da array cada entry (0-1) (2-3) (4-5) ... (10-11)
+        j=0
         for i in range(0,6): #dentro do frame criado - - no final salvar na lista encadeada
-            j=1
             #label entrada
             texto=Label(self.frameBombas[i], text=f"B{i})")
             texto.grid(row=1,column=0)
-            
             #entrada
             entradas1=Entry(self.frameBombas[i])
             entradas1.grid(row=1,column=1)
             # no momento em que tira o foco da caixinha ele salva o valor indo la pra funcao de pegar os dados
-            entradas1.bind("<FocusOut>", lambda event,contador=i,entrada=entradas1 : self.pegarOsDados(contador, entrada))
-            
+            entradas1.bind("<FocusOut>", lambda event,contador=i+j,entrada=entradas1 : self.pegarOsDados(contador, entrada))
+            j+=1
             entradas2=Entry(self.frameBombas[i])
-            entradas2.grid(row=1,column=1)
+            entradas2.grid(row=2,column=1)
             # no momento em que tira o foco da caixinha ele salva o valor indo la pra funcao de pegar os dados
-            entradas2.bind("<FocusOut>", lambda event,contador=i+1,entrada=entradas2 : self.pegarOsDados(contador, entrada))
+            entradas2.bind("<FocusOut>", lambda event,contador_2=i+j,entrada_2=entradas2 : self.pegarOsDados(contador_2, entrada_2))
             
 
         #botao de execucao do programa    
@@ -65,11 +64,7 @@ class Aplicativo():
         #salvando na array
         tempValor= entrada.get()
         if tempValor!="":
-            tempFloatValor=float(tempValor)
-            if tempFloatValor%2==0:
-                self.entradasTotal[i]=tempFloatValor
-            else:
-                self.entradasTotal[i]=tempFloatValor
+            self.entradasTotal[i]=float(tempValor)
         else:
             print("VAZIO")
 
