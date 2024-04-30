4debbacb8b13d2b4d45270317f8260710f6c3105
Allyson-SFelix <allysonstudy2021@gmail.com>
2024-04-30 11:53:30 -0300

    Projeto finalizado, correcoes do commit anterior realizado com sucesso

--- funcoesFuncionalidades.py
+++ funcoesFuncionalidades.py
@@ -1,35 +1,37 @@
-from funcoesSobDados import *
+
 def trabalharNosDados(listaBombas,Bombas):
-    litrosFinaisPorBomba=[0]*6
-    dinheiroPorBombaDosLitros=[0]*6
-    dinheiroPorBombaTotal=[0]*6
+    litrosTotalPorBomba=[] #litro no total apos reduzir com litro revenda
+    dinheiroPorBombaDosLitros=[]
+    dinheiroPorBombaTotal=[]
     dinheiroTotalBombas=0
+
     for i in range(0,6):
-        litrosFinaisPorBomba.append(somaLitroFinal(i,listaBombas))
-        dinheiroPorBombaDosLitros.append(somaDinheiroPorBomba(i,listaBombas,litrosFinaisPorBomba))
+        litrosTotalPorBomba.append(somaLitroFinal(i,listaBombas))
+        dinheiroPorBombaDosLitros.append(somaDinheiroPorBomba(i,listaBombas,litrosTotalPorBomba))
         dinheiroPorBombaTotal.append(somaDosDinheiros(i,listaBombas,dinheiroPorBombaDosLitros))
         dinheiroTotalBombas+=dinheiroPorBombaTotal[i]
     
     valorCaixa=Bombas.dinheiroCaixa + Bombas.valesCaixa
-    diferencaDinheiro=dinheiroTotalBombas - valorCaixa
-    print(valorCaixa,diferencaDinheiro,dinheiroTotalBombas)
+    diferencaDinheiro=0.0
+    diferencaDinheiro=valorCaixa-dinheiroTotalBombas 
     escreverNoArquivo(valorCaixa,diferencaDinheiro,dinheiroTotalBombas)
     
     
-def escreverNoArquivo(valorCaixa,diferencaDinheiro,dinheiroTotalBombas):
-    with open("resultadoCaixa.txt","w") as arquivo:
-        arquivo.write(f"Valor do dinheiro do caixa do dia: {dinheiroTotalBombas}\nValor do dinheiro das Bombas: {valorCaixa}\nDiferenca ( Se for positivo -> ESTA PASSANDO \t Se for negativo -> ESTA FALTANDO): {diferencaDinheiro}")
-    
 def somaLitroFinal(contador,listaBombas):
     tempPorBombas=listaBombas[contador].litroTotalMedido - listaBombas[contador].RvLitro 
-    print(tempPorBombas)
     return tempPorBombas
 
 def somaDinheiroPorBomba(contador, listaBombas,litrosPorBomba):
     tempValores=litrosPorBomba[contador] * listaBombas[contador].valorLitro
-    print(tempValores)
     return tempValores
 
 def somaDosDinheiros(contador,listaBombas,dinheiroPorBombasLitros):
     tempDinheiroBomba=listaBombas[contador].RvDinheiro + dinheiroPorBombasLitros[contador]
     return tempDinheiroBomba
+
+
+def escreverNoArquivo(valorCaixa,diferencaDinheiro,dinheiroTotalBombas):
+    with open("resultadoCaixa.txt","w") as arquivo:
+        arquivo.write(f"Valor do dinheiro do caixa do dia: {valorCaixa:.2f}\n")
+        arquivo.write(f"Valor do dinheiro das Bombas: {dinheiroTotalBombas:.2f}\n")
+        arquivo.write(f"Diferenca: \n\t( Se for positivo -> ESTA PASSANDO \t Se for negativo -> ESTA FALTANDO):\n{diferencaDinheiro:.2f}")
--- funcoesSobDados.py
+++ funcoesSobDados.py
@@ -6,14 +6,13 @@ class Bombas:
         self.litroTotalMedido=0.0
         self.RvDinheiro=0.0
         self.RvLitro=0.0
-        self.dinheiroCaixa=0
-        self.valesCaixa=0
+        self.dinheiroCaixa=0.0
+        self.valesCaixa=0.0
         
     @classmethod
     def salvarValoresCaixa(cls,dinheiro,vale):
                 cls.dinheiroCaixa=dinheiro
                 cls.valesCaixa=vale
-                print(cls.dinheiroCaixa,cls.valesCaixa)
 
    
     
@@ -30,11 +29,10 @@ def salvarDados(litroMedida,valorLitro,dinheiroRevenda,litrosRevenda,dinheiroCai
         trabalharNosDados(listaBombas,Bombas)
 
 def salvarDadosRevenda(listaBombas,dinheiroRevenda,litrosRevenda):
-    for i in range(6): 
-        listaBombas[i].RvLitros=litrosRevenda[i]
+    for i in range(0,6): 
+        listaBombas[i].RvLitro=litrosRevenda[i]
         listaBombas[i].RvDinheiro=dinheiroRevenda[i]
-        print(listaBombas[i].RvLitros)
-        print(listaBombas[i].RvDinheiro)
+        
 
 
 def salvarLitroMedidos(listaBombas,litroMedidaEntrada):    
@@ -43,13 +41,11 @@ def salvarLitroMedidos(listaBombas,litroMedidaEntrada):
         litroTempManha=litroMedidaEntrada[i+j]
         j+=1
         litroTempNoite=litroMedidaEntrada[i+j] 
-        listaBombas[i].litroTotalMedido=litroTempManha+litroTempNoite
-        print(listaBombas[i].litroTotalMedido)
+        listaBombas[i].litroTotalMedido=abs(litroTempManha-litroTempNoite)
             
             
 def salvarValorLitros(listaBombas,valorLitro):
     for i in range(6): 
         listaBombas.append(Bombas())
         listaBombas[i].valorLitro=valorLitro[i]
-        print(listaBombas[i].valorLitro)
         
